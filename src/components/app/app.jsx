4fcb63990d1fc6ad22a57e1776cc21167190d21a
Adilek96 <ashurovadil96@gmail.com>
2023-06-26 23:15:30 +0400

    adding employees delete functional

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -6,30 +8,45 @@ import EmployeesAddForm from "../employees-add-form/employers-add-form";
 
 import "./app.css";
 
-function App(){
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            data : [
+                {name: "John S.", salary: 800, increase: true, id: 1},
+                {name: "Nikola T.", salary: 2000, increase: false, id: 2},
+                {name: "Nikon S.", salary: 1000, increase: false, id: 3},
+                {name: "Ilon M.", salary: 100, increase: false, id: 4},
+                {name: "Arnold Sw.", salary: 1500, increase: false, id: 5 }
+            ]
+        }
+    }
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data:  data.filter(item =>  item.id !== id)
+            }
+        })
+    }
+    render(){
     
-    const data = [
-        {name: "John S.", salary: 800, increase: true, id: 1},
-        {name: "Nikola T.", salary: 2000, increase: false, id: 2},
-        {name: "Nikon S.", salary: 1000, increase: false, id: 3},
-        {name: "Ilon M.", salary: 100, increase: false, id: 4},
-        {name: "Arnold Sw.", salary: 1500, increase: false, id: 5 }
-    ]
-
-
-    return(
-        <div className="app">
-            <AppInfo />
-
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+        return(
+            <div className="app">
+                <AppInfo />
+    
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
+    
+                <EmployeesList data={this.state.data} onDelete={this.deleteItem}/> 
+                <EmployeesAddForm /> 
             </div>
-
-            <EmployeesList data={data}/> 
-            <EmployeesAddForm /> 
-        </div>
-    )
+        )
+    }
+    
 }
 
 
